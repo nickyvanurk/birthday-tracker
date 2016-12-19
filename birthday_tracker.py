ed83f04e0a7d230bb1f82088fb7b7fba4250d6b7
nickyvanurk <nickyvanurk@gmx.com>
2016-12-19 13:04:19 +0100

    Added command line arguments

--- birthday_tracker.py
+++ birthday_tracker.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-import contextlib
+import contextlib, argparse, collections
+
+def get_command_line_args():
+  defaults = {'name': None, 'birthdate': None}
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-n', dest='name', help='name of person')
+  parser.add_argument('-b', dest='birthdate', help='date of birth')
+  command_line_args = {k:v for k, v in vars(parser.parse_args()).items() if v}
+  return collections.ChainMap(command_line_args, defaults)
 
 def main():
-  pass
+  args = get_command_line_args()
+
+  if args['name'] and args['birthdate']:
+    print(args['name'] + ', ' + args['birthdate'])
+  else:
+    print('Display birthdays list')
 
 if __name__ == '__main__':
   with contextlib.suppress(KeyboardInterrupt):
--- test_birthday_tracker.py
+++ test_birthday_tracker.py
@@ -1,4 +1,6 @@
 import birthday_tracker
 
-def test():
-  return True
+def test_get_command_line_args():
+  args = birthday_tracker.get_command_line_args()
+  assert args['name'] == None
+  assert args['birthdate'] == None
