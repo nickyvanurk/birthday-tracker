30a33c29308db6640d1401ef2083e141669dfc33
nickyvanurk <nickyvanurk@gmx.com>
2016-12-20 18:24:21 +0100

    Added birthdate validation

--- birthday_tracker.py
+++ birthday_tracker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import contextlib, argparse, collections
+import contextlib, argparse, collections, datetime
 
 def get_command_line_args():
   defaults = {'name': None, 'birthdate': None}
@@ -9,11 +9,24 @@ def get_command_line_args():
   command_line_args = {k:v for k, v in vars(parser.parse_args()).items() if v}
   return collections.ChainMap(command_line_args, defaults)
 
+def is_date_valid(birthdate):
+  try:
+    bdate = birthdate.split('-')
+    day = int(bdate[0])
+    month = int(bdate[1])
+    year = int(bdate[2])
+    date = datetime.datetime(year,month,day)
+  except ValueError:
+    return False
+  return date <= date.now()
+
 def main():
   args = get_command_line_args()
-
   if args['name'] and args['birthdate']:
-    print(args['name'] + ', ' + args['birthdate'])
+    if is_date_valid(args['birthdate']):
+      print(args['name'] + ', ' + args['birthdate'])
+    else:
+      print('Invalid birthday')
   else:
     print('Display birthdays list')
 
--- test_birthday_tracker.py
+++ test_birthday_tracker.py
@@ -4,3 +4,12 @@ def test_get_command_line_args():
   args = birthday_tracker.get_command_line_args()
   assert args['name'] == None
   assert args['birthdate'] == None
+
+def test_is_date_valid():
+  assert birthday_tracker.is_date_valid('') == False
+  assert birthday_tracker.is_date_valid('0-0-0') == False
+  assert birthday_tracker.is_date_valid('1-1-2016') == True
+  assert birthday_tracker.is_date_valid('01-01-2016') == True
+  assert birthday_tracker.is_date_valid('32-01-2016') == False
+  assert birthday_tracker.is_date_valid('10-13-2016') == False
+  assert birthday_tracker.is_date_valid('10-01-2030') == False
